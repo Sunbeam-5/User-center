6834d9078613270293e834d6084b9e8fc6ebf02f
Sunbeam-5 <90610326+Sunbeam-5@users.noreply.github.com>
2024-04-18 17:27:38 +0800

    初始化

---
+++ src/main/java/com/zh/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.zh.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zh.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
